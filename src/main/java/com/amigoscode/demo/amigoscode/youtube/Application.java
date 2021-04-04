49e3d1fcaf224276ccae380dbae26c285926add0
HP <bilimus@gmail.com>
2021-04-04 13:11:44 +0500

    StudentController

--- src/main/java/com/amigoscode/demo/amigoscode/youtube/Application.java
+++ src/main/java/com/amigoscode/demo/amigoscode/youtube/Application.java
@@ -1,10 +1,13 @@
 package com.amigoscode.demo.amigoscode.youtube;
 
+import com.amigoscode.demo.amigoscode.youtube.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,14 +19,36 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
+	@GetMapping
 	public String hello(){
 		return "Hello World";
 	}
 
-	@GetMapping
+	@GetMapping("/list")
 	public List<String> hellolist(){
 		return List.of("Hello", "list");
 	}
 
+	@GetMapping("/class")
+	public List<Student> helloStudent(){
+		return List.of(new Student(
+				1L,
+				"@Dude_Alex",
+				"dudealex@gmail.com",
+				LocalDate.of(1921, Month.JANUARY,1),
+				100));
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/amigoscode/demo/amigoscode/youtube/student/StudentController.java
@@ -0,0 +1,4 @@
+package com.amigoscode.demo.amigoscode.youtube.student;
+
+public class StudentController {
+}
