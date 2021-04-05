1674ec883f823fa05e2a27a6daea855845e89775
HP <bilimus@gmail.com>
2021-04-05 07:43:19 +0500

    pom changed

--- src/main/java/com/amigoscode/demo/amigoscode/youtube/student/Student.java
+++ src/main/java/com/amigoscode/demo/amigoscode/youtube/student/Student.java
@@ -1,8 +1,21 @@
 package com.amigoscode.demo.amigoscode.youtube.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
